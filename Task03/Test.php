91a57a3de43caf37b86ebad029e96c65d09b1132
1D4n3 <125080408+1D4n3@users.noreply.github.com>
2023-03-31 21:16:08 +0300

    Task03

---
+++ Task03/Test.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Student;
+use App\StudentsList;
+
+function runTest()
+{
+    $st1 = new StudentsList(array());
+    $s1 = new Student();
+    $s1->setLastname("Иванов")
+    ->setFirstname("Иван")
+    ->setFaculty("ФМиИТ")
+    ->setGroup(102);
+    echo $s1 . PHP_EOL;
+    $st1->add($s1);
+    echo "Количество человек в списке: " . $st1->count() . PHP_EOL;
+
+    $s2 = new Student();
+    $s2->setLastname("Вельмякина")
+    ->setFirstname("Олеся")
+    ->setFaculty("ИФХ")
+    ->setGroup(305);
+    echo $s2 . PHP_EOL;
+    $st1->add($s2);
+    echo "Количество человек в списке: " . $st1->count() . PHP_EOL;
+
+    $s3 = new Student();
+    $s3->setLastname("Курганов")
+    ->setFirstname("Илья")
+    ->setFaculty("ИЭС")
+    ->setGroup(401);
+    echo $s3 . PHP_EOL;
+    $st1->add($s3);
+    echo "Количество человек в списке: " . $st1->count() . PHP_EOL;
+	
+    $st1->store("StudentsList.bin");
+    echo "\nStudentsList0:" . PHP_EOL;
+    
+    for ($j = 0; $j < $st1->count(); $j++) {
+        echo $st1->get($j) . PHP_EOL;
+    }
+
+    $st2 = new StudentsList(array());
+	echo "\nStudentsList1:" . PHP_EOL;
+    echo "\nКоличество человек в списке: " . $st2->count() . PHP_EOL;
+
+    $st2->load("StudentsList.bin");
+	echo "\nStudentsList2:" . PHP_EOL;
+    echo "\nКоличество человек в списке: " . $st2->count() . PHP_EOL;
+    for ($j = 0; $j < $st2->count(); $j++) {
+        echo $st2->get($j) . PHP_EOL;
+    }
+}
---
+++ Task03/run_test.php
@@ -0,0 +1,5 @@
+<?php
+require_once("Student.php");
+require_once("StudentsList.php");
+require_once("Test.php");
+runTest();
